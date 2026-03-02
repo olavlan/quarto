1d37f23d1a82894bbd6b2ebd4d1aeb9d2ad2e009
Olav Landsverk <olav.landsverk@gmail.com>
2026-03-02 16:46:55 +0100

    updates
    Signed-off-by: Olav Landsverk <olav.landsverk@gmail.com>

--- screenshot.py
+++ screenshot.py
@@ -1,12 +1,57 @@
-# /// script
-# dependencies = ["playwright"]
-# ///
-
 import asyncio
 from pathlib import Path
 from playwright.async_api import async_playwright
+import subprocess
+import jinja2
+import shutil
+import pathlib
 
-FLAVORS = ["mocha"]
+QUARTO_PROJECT_FOLDER = pathlib.Path("quarto-project")
+QUARTO_CONFIG_PATH = QUARTO_PROJECT_FOLDER / "_quarto.yml"
+QUARTO_INDEX_PATH = QUARTO_PROJECT_FOLDER / "index.qmd"
+FLAVORS = ["latte", "frappe", "macchiato", "mocha"]
+
+
+def create_quarto_website_project(flavor: str):
+    subprocess.run(
+        [
+            "quarto",
+            "create",
+            "project",
+            "website",
+            str(QUARTO_PROJECT_FOLDER),
+            "--no-prompt",
+            "--no-open",
+        ],
+        check=True,
+    )
+
+    QUARTO_INDEX_PATH.unlink()
+    QUARTO_CONFIG_PATH.unlink()
+
+    jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
+    quarto_configuration_template = jinja_environment.get_template("_quarto.yml")
+
+    with open(QUARTO_CONFIG_PATH, "w") as configuration_file:
+        configuration_file.write(quarto_configuration_template.render(flavor=flavor))
+
+    shutil.copy("index.qmd", QUARTO_INDEX_PATH)
+
+    style = f"catppuccin-{flavor}.scss"
+    shutil.copy(f"themes/{style}", QUARTO_PROJECT_FOLDER / style)
+
+
+def render_quarto_project(flavor: str):
+    subprocess.run(
+        [
+            "quarto",
+            "render",
+            QUARTO_PROJECT_FOLDER,
+            "--to",
+            "html",
+        ],
+        check=True,
+    )
 
 
 async def capture_screenshot(flavor: str):
@@ -14,7 +59,7 @@ async def capture_screenshot(flavor: str):
         browser_instance = await playwright_manager.chromium.launch()
         browser_page = await browser_instance.new_page()
 
-        absolute_file_path = Path("test/_site/index.html").resolve()
+        absolute_file_path = Path(f"{QUARTO_PROJECT_FOLDER}/_site/index.html").resolve()
 
         await browser_page.goto(f"file://{absolute_file_path}")
         await browser_page.screenshot(path=f"assets/{flavor}.png")
@@ -23,4 +68,7 @@ async def capture_screenshot(flavor: str):
 
 if __name__ == "__main__":
     for flavor in FLAVORS:
+        create_quarto_website_project(flavor)
+        render_quarto_project(flavor)
         asyncio.run(capture_screenshot(flavor))
+        shutil.rmtree(QUARTO_PROJECT_FOLDER)
